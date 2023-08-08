f57558ed9d64d71db412f0016eb0d5c3fc5a3f0e
Emanuel Moreno <memoclan92@gmail.com>
2023-08-08 02:45:49 -0600

    commit two

--- django_api/settings.py
+++ django_api/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["https://emanuel-moreno.github.io/React-Django-App"]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
